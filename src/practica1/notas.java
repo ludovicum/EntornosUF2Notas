db92d0479e21f6189a5bb6056b421f7669f671a2
ludovicum <luiscamargocandelas@gmail.com>
2022-04-07 21:34:06 +0200

    Cambio de nombre de variables

--- src/practica1/notas.java
+++ src/practica1/notas.java
@@ -14,7 +14,7 @@ public class notas {
 	/**
 	 * Declaración de variables
 	 */
-	double uf1, uf2, uf3;
+	double nota1, nota2, nota3;
 	double acu1, acu2, acu3, def;
 
 	Scanner entrada = new Scanner(System.in);
@@ -30,15 +30,15 @@ public class notas {
 
 		System.out.print("ingrese nota 1: ");
 
-		uf1 = entrada.nextDouble();
+		nota1 = entrada.nextDouble();
 
 		System.out.print("ingrese nota 2: ");
 
-		uf2 = entrada.nextDouble();
+		nota2 = entrada.nextDouble();
 
 		System.out.print("ingrese nota 3: ");
 
-		uf3 = entrada.nextDouble();
+		nota3 = entrada.nextDouble();
 
 	}
 
@@ -47,20 +47,20 @@ public class notas {
 	 */
 	public void comprobacion() {
 
-		if (uf1 > 10) {
+		if (nota1 > 10) {
 			System.out.println(" nota1 mal introducida");
 
 		} else {
 			System.out.println(" nota1 correcta");
 		}
 
-		if (uf2 > 10) {
+		if (nota2 > 10) {
 			System.out.println(" nota2 mal introducida");
 
 		} else {
 			System.out.println(" nota2 correcta");
 		}
-		if (uf3 > 10) {
+		if (nota3 > 10) {
 			System.out.println(" nota3 mal introducida");
 
 		} else {
@@ -74,9 +74,9 @@ public class notas {
 	 * cada UF y la suma de todas para obtener la nota definitiva.
 	 */
 	public void Calculonotas() {
-		acu1 = uf1 * 0.35;
-		acu2 = uf2 * 0.35;
-		acu3 = uf3 * 0.30;
+		acu1 = nota1 * 0.35;
+		acu2 = nota2 * 0.35;
+		acu3 = nota3 * 0.30;
 
 		def = acu1 + acu2 + acu3;
 
@@ -89,9 +89,9 @@ public class notas {
 	public void Mostrar() {
 
 		System.out.println(" notas introducidas son:");
-		System.out.println(" nota1 = " + uf1);
-		System.out.println(" nota2 = " + uf2);
-		System.out.println(" nota3 = " + uf3);
+		System.out.println(" nota1 = " + nota1);
+		System.out.println(" nota2 = " + nota2);
+		System.out.println(" nota3 = " + nota3);
 
 		System.out.println(" acumuado 1 = " + acu1);
 		System.out.println(" acumuado 2 = " + acu2);
